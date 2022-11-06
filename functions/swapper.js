e2bb45339b9c073ecbced6ceac6c1ab3a0638319
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-06 14:21:45 -0500

    update

--- functions/swapper.js
+++ functions/swapper.js
@@ -17,5 +17,5 @@ const swapper = function (key1, object1, key2, object2) {
 swapper("a", { a: 1, b: 2, c: 3 }, "c", { a: 4, b: 3, c: 5 });
 swapper("b", { a: 8, b: 7, c: 6 }, "d", { a: 5, b: 1, c: 2, d: 12 });
 swapper("c", { a: 1, b: 3, c: 3, d: 7 }, "c", { a: 4, b: 0, c: 5 });
-swapper("b", { c: 3 }, "b", { a: 4, b: 0, c: 5, d: 1 });
-swapper("b", { a: 4, b: 0, c: 5, d: 1 });
+
+//Co-authored-by: Elshan Magsudov <eliomags@users.noreply.github.com>
